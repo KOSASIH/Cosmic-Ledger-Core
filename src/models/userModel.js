1843611a14d3264c062ca346bbb7c05b64939f34
KOSASIH <kosasihg88@gmail.com>
2024-12-24 12:55:40 +0700

    Create userModel.js

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+// src/models/userModel.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const User = mongoose.model('User ', userSchema);
+module.exports = User;
