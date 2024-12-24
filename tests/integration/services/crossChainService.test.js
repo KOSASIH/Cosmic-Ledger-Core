085c0d2b7bbc3ece461dbeac2df292014aa1a133
KOSASIH <kosasihg88@gmail.com>
2024-12-24 17:09:48 +0700

    Create crossChainService.test.js

---
+++ tests/integration/services/crossChainService.test.js
@@ -0,0 +1,16 @@
+// tests/integration/services/crossChainService.test.js
+
+const crossChainService = require('../../src/services/crossChainService');
+
+describe('CrossChain Service', () => {
+    it('should initiate a cross-chain transfer', async () => {
+        const response = await crossChainService.initiateTransfer('0xAddress', 100, 'Ethereum');
+        expect(response).toHaveProperty('status', 'Transfer initiated');
+    });
+
+    it('should complete a cross-chain transfer', async () => {
+        await crossChainService.initiateTransfer('0xAddress', 100, 'Ethereum');
+        const response = await crossChainService.completeTransfer('0xAddress', 100, 'Ethereum');
+        expect(response).toHaveProperty('status', 'Transfer completed');
+    });
+});
